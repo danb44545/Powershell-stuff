b2b5f1f0ab9e0b3eb40cde6347ed0c8253db1654
xxdanbrowne <31517279+danb44545@users.noreply.github.com>
2021-03-15 16:12:24 -0600

    Update updategraylogsinglerecord.ps1
    Make it pull data from the windows event log "system".
    Single record POC.
    TODO: Need code to convert datetime record into unix utc timestamp

--- updategraylogsinglerecord.ps1
+++ updategraylogsinglerecord.ps1
@@ -1,7 +1,28 @@
+
+
+Get-EventLog system | select -First 1 | ForEach-Object {
+$i = $($_.Index)
+$t = $($_.Time)
+$et = $($_.EntryType)
+$s = $($_.Source)
+$iid = $($_.InstanceID)
+$m = $($_.Message)
+
+Write-Host "message = $m"
+
+$h = "dominoes8"
+
 $param = @{
     Uri         = "http://10.0.0.253:12201/gelf"
     Method      = "Post"
-    Body        = '{"version":"1.1","host":"dominoes","short_message":"more pizza?","level":"5"}'
+    Body        = '{"version":"1.1","host":"' + $h  +  '","short_message":"' + $m + '","level":"5","EntryType":"' + $et + '","EventSource":"' + $s + '"}'
     ContentType = "application/json"
 }
+
+
 Invoke-RestMethod @param
+
+}
+
+
+
